ea234b474dbff554f5690e0406c6db4d2d6b4748
areebalii <areebktk8@gmail.com>
2025-10-10 12:42:01 +0500

    Refactor app and index files: move server initialization to index.js and clean up app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,6 @@
 import express from 'express';
-
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
 
-app.get('/', (req, res) => {  
-  res.send('Hello World!')
-})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-})
+export { app }
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
 import connectDb from './database/connectDb.js';
+import { app } from './app.js';
 
 dotenv.config();
 
 
-connectDb();
+connectDb()
+.then(() => {
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT || 5000}`);
+  });
+})
+.catch((error) => {
+  console.error("Failed to connect to the database:", error);
+  process.exit(1);
+});
+
