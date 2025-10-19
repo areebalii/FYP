ed46102a6784e32ad916c45be4f33e808e674d7f
areebalii <areebktk8@gmail.com>
2025-10-19 17:55:07 +0500

    Refactor asyncHandler utility to use Promise.resolve for better error handling

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,10 +1,18 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    
-  } catch (error) {
-    res.status(error.code || 500).json({ message: error.message || "Internal Server Error" });
-  } 
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next); 
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal Server Error"
+//     });
+//   }
+// }
 
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+  }
 }
 
 export { asyncHandler }
