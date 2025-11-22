5cd8112b38e4329bcbcabaa8f9bc3f0c0aea89bf
areebalii <areebktk8@gmail.com>
2025-11-22 13:33:00 +0500

    Add user registration route and controller for user registration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,5 +13,11 @@ app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 
+// import routes
+import userRouter from './routes/user.route.js';
+
+// route declaration
+app.use('/api/users', userRouter)
+
 
 export { app }
--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,9 +1,8 @@
-import asyncHandler from "../utils/asyncHandler.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
-const registerUser = asyncHandler(async (req, res) => {
+export const registerUser = asyncHandler(async (req, res) => {
   res.send("User registered")
-  res.status(200).json({
-    success: true,
+  res.status(200).json({ 
     message: "User registered successfully"
   })
 })
