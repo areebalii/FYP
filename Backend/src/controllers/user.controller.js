4e73b9ff14da6c77a4356b0f864cc903531c8bb3
areebalii <areebktk8@gmail.com>
2025-11-24 19:52:26 +0500

    Add Cloudinary integration and update user registration controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,8 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 export const registerUser = asyncHandler(async (req, res) => {
-  res.send("User registered")
-  res.status(200).json({ 
-    message: "User registered successfully"
-  })
+  //  get user details from frontend
+  // validation
+  //  check if user already exists
+  // hash password
+  // create user object
+  // save user to database
+  // send response
+
+  const { username, fullName, email, password, address, avatar } = req.body;
+  console.log("Email received:", email);
+  
+  
 })
