7943909413cda70d424ecd5b4b9148df19abccdc
areebalii <areebktk8@gmail.com>
2025-11-30 20:21:33 +0500

    Refactor user registration to include avatar upload and improve validation checks

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,9 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
 
 export const registerUser = asyncHandler(async (req, res) => {
-  //  get user details from frontend
+  // get user details from frontend
   // validation
-  //  check if user already exists
+  // check if user already exists
   // hash password
   // create user object
   // save user to database
@@ -11,6 +12,17 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   const { username, fullName, email, password, address, avatar } = req.body;
   console.log("Email received:", email);
-  
-  
+
+  // traditional way of checking each field
+  // if(fullName === "") {
+  //   throw new ApiError(400, "All fields are required")
+  // }
+
+  // advanced way of checking all fields
+  if (
+    [username, fullName, email, password, address].some((fields) => fields?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required")
+  }
+
 })
