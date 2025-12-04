bf0bf81b502ee79cdaeed85fc320914b20d60366
areebalii <areebktk8@gmail.com>
2025-12-04 19:24:34 +0500

    Refactor user model methods for consistency and add missing refresh token generation

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -55,17 +55,17 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true });
 
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 })
 
-userSchema.methods.isPasswordCorrect = async function(password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateAccessToken = function() {
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -75,6 +75,14 @@ userSchema.methods.generateAccessToken = function() {
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
   )
 }
-userSchema.methods.generateRefreshToken = function() {}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  )
+}
 
 export const User = mongoose.model('User', userSchema);
