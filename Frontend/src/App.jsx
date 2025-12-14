b6757151148dea23131494507c223372a3e015dd
areebalii <areebktk8@gmail.com>
2025-12-14 15:05:42 +0500

    feat: add new pages for About, Contact, Product, and Cart with routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,10 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Collections from './pages/Collections'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
 
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collections' element={<Collections />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
 
     </div>
